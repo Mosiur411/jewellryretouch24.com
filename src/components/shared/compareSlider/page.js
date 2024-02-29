dbaa89e87166f6e6f7711e7c96f1586779ae6390
Mosiur411 <mosiurislamwebdesign@gmail.com>
2024-02-29 17:45:19 +0600

    add

--- src/components/shared/compareSlider/page.js
+++ src/components/shared/compareSlider/page.js
@@ -4,9 +4,9 @@ export default function CompareSlider({ before, after, retundata }) {
     return (
         <div className="h-full w-full" >
             <ReactCompareSlider
+                className={`bg-red-500 h-full md:rounded-t-none rounded-t-xl  ${retundata ? ' md:rounded-r-xl' : 'md:rounded-l-xl md:rounded-tl-xl'} `}
                 itemOne={<ReactCompareSliderImage src={before} srcSet={before} alt="Image one" />}
                 itemTwo={<ReactCompareSliderImage src={after} srcSet={after} alt="Image two" />}
-                className={`h-full md:rounded-t-none rounded-t-xl  ${retundata ? ' md:rounded-r-xl' : 'md:rounded-l-xl md:rounded-tl-xl'} `}
             />
         </div>
     );
