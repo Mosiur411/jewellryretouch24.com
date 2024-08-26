dea68b76f6f0217c4bfcbb24d0c87d30f96d0c2c
Mosiur411 <mosiurislamwebdesign@gmail.com>
2024-08-26 16:57:37 +0600

    ad

--- src/components/shared/compareSlider/page.js
+++ src/components/shared/compareSlider/page.js
@@ -4,7 +4,7 @@ export default function CompareSlider({ before, after, retundata }) {
     return (
         <div className="h-full w-full" >
             <ReactCompareSlider
-                className={`bg-red-500 h-full md:rounded-t-none rounded-t-xl  ${retundata ? ' md:rounded-r-xl' : 'md:rounded-l-xl md:rounded-tl-xl'} `}
+                className={`h-full md:rounded-t-none rounded-t-xl  ${retundata ? ' md:rounded-r-xl' : 'md:rounded-l-xl md:rounded-tl-xl'} `}
                 itemOne={<ReactCompareSliderImage src={before} srcSet={before} alt="Image one" />}
                 itemTwo={<ReactCompareSliderImage src={after} srcSet={after} alt="Image two" />}
             />
--- src/components/shared/navbar/menu.js
+++ src/components/shared/navbar/menu.js
@@ -37,7 +37,7 @@ export default function Menu() {
                     </Link>
 
                     {/* Services submenu */}
-                    <div onMouseEnter={() => setSubmenu(true)} onMouseLeave={() => setSubmenu(false)} className={`absolute rounded  -left-40 lg:w-96 w-80 text-[14px] font-medium bg-red-300 flex flex-col transition-all ease-linear duration-100 ${submenu ? "visible top-[3.8rem] opacity-100" : "invisible top-[4.5rem] opacity-0"} `}>
+                    <div onMouseEnter={() => setSubmenu(true)} onMouseLeave={() => setSubmenu(false)} className={`absolute rounded  -left-40 lg:w-96 w-80 text-[14px] font-medium  flex flex-col transition-all ease-linear duration-100 ${submenu ? "visible top-[3.8rem] opacity-100" : "invisible top-[4.5rem] opacity-0"} `}>
                         <div className='relative'>
                             <Link
                                 onMouseEnter={() => setSubItem(true)}
@@ -47,7 +47,7 @@ export default function Menu() {
                             </Link>
 
                             {/* For submenu submenus */}
-                            <div onMouseEnter={() => setSubItem(true)} onMouseLeave={() => setSubItem(false)} className={`absolute rounded  lg:-right-96 -right-80 lg:w-96 w-80 text-[14px] font-medium bg-red-300 flex flex-col transition-all ease-linear duration-150 ${subItem ? "visible top-0 opacity-100" : "invisible top-[.2rem] opacity-0"} `}>
+                            <div onMouseEnter={() => setSubItem(true)} onMouseLeave={() => setSubItem(false)} className={`absolute rounded  lg:-right-96 -right-80 lg:w-96 w-80 text-[14px] font-medium  flex flex-col transition-all ease-linear duration-150 ${subItem ? "visible top-0 opacity-100" : "invisible top-[.2rem] opacity-0"} `}>
                                 <Link
                                     className='py-3 px-3 w-full bg-gray-200 dark:bg-[#1c1c1c] dark:text-gray-300 hover:bg-gray-300 dark:hover:bg-[#00000094] dark:border-gray-600 border-b '
                                     href={'/services/brass-ligth'}>Brass Light 
@@ -84,7 +84,7 @@ export default function Menu() {
                             </Link>
 
                             {/* For submenu submenus */}
-                            <div onMouseEnter={() => OthersetSubItem(true)} onMouseLeave={() => OthersetSubItem(false)} className={`absolute rounded  lg:-right-96 -right-80 lg:w-96 w-80 text-[14px] font-medium bg-red-300 flex flex-col transition-all ease-linear duration-150 ${OthersubItem ? "visible top-0 opacity-100" : "invisible top-[.2rem] opacity-0"} `}>
+                            <div onMouseEnter={() => OthersetSubItem(true)} onMouseLeave={() => OthersetSubItem(false)} className={`absolute rounded  lg:-right-96 -right-80 lg:w-96 w-80 text-[14px] font-medium  flex flex-col transition-all ease-linear duration-150 ${OthersubItem ? "visible top-0 opacity-100" : "invisible top-[.2rem] opacity-0"} `}>
                                 <Link
                                     className='py-3 px-3 w-full bg-gray-100 dark:bg-[#1c1c1c] dark:text-gray-300 hover:bg-gray-200 dark:hover:bg-[#00000094] dark:border-gray-600 border-b '
                                     href={'/services/clipping-path-service'}>Photo Retouching Service
